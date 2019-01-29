1c1037c16f4d28f403ac9d3ecdcc2e06ca6c57b0
Sabas <s@theinventorhouse.org>
2019-01-29 15:23:28 +0100

    Update lora.cpp

--- lora.cpp
+++ lora.cpp
@@ -409,7 +409,7 @@ void send(String a)
   writeRegister(REG_FIFO_ADDR_PTR, 0);
   writeRegister(REG_PAYLOAD_LENGTH, 0);
 
-  if( a->getUTF8Data()[0]  ==  0)return;
+  if( a->charCodeAt(0) ==  0)return;
   for(int i=0; i<254 ;i++){
     intSend =  a->getUTF8Data()[i];
     if(intSend  ==  0 && i == 0)return;
